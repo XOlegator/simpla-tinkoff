43e9321a9b670434701ed0ea9e58cd023af2ed5d
Oleg <subspam@mail.ru>
2017-03-25 11:18:47 +0300

    При успешной оплате. Выводится сообщение об этом и ссылка на страницу заказа.

--- Tinkoff.php
+++ Tinkoff.php
@@ -7,37 +7,47 @@ class Tinkoff extends Simpla
 {
     public function checkout_form($order_id, $button_text = null)
     {
-        if(empty($button_text))
-            $button_text = 'Перейти к оплате';
+        if (empty($button_text)) {
+            $button_text = 'Перейти к оплате &#8594;';
+        }
 
         $button = '';
         $order = $this->orders->get_order((int)$order_id);
 
-        if ($order->status == 0) {
-            $payment_method = $this->payment->get_payment_method($order->payment_method_id);
-            $payment_currency = $this->money->get_currency(intval($payment_method->currency_id));
-            $settings = $this->payment->get_payment_settings($payment_method->id);
+        $payment_method = $this->payment->get_payment_method($order->payment_method_id);
+        $payment_currency = $this->money->get_currency(intval($payment_method->currency_id));
+        $settings = $this->payment->get_payment_settings($payment_method->id);
 
-            $price = round($this->money->convert($order->total_price, $payment_method->currency_id, false), 2);
+        $price = round($this->money->convert($order->total_price, $payment_method->currency_id, false), 2);
 
-            // описание заказа
-            $desc = 'Оплата заказа №'.$order->id;
-            $config = new Config();
-            $arrFields = array(
-                'OrderId'  => $order->id,
-                'Amount'   => $price * 100,
-                'DATA'	   => 'Email='.$order->email.'|connection_type=simpla'.$config->version
-            );
+        // описание заказа
+        $desc = 'Оплата заказа №'.$order->id;
+        $config = new Config();
+        $arrFields = array(
+            'OrderId'  => $order->id,
+            'Amount'   => $price * 100,
+            'DATA'	   => 'Email='.$order->email.'|connection_type=simpla'.$config->version
+        );
 
-            $Tinkoff = new TinkoffMerchantAPI( $settings['tinkoff_terminal'], $settings['tinkoff_secret'], $settings['tinkoff_gateway'] );
-            $request = $Tinkoff->buildQuery('Init', $arrFields);
-            $request = json_decode($request);
+        $Tinkoff = new TinkoffMerchantAPI( $settings['tinkoff_terminal'], $settings['tinkoff_secret'], $settings['tinkoff_gateway'] );
+        $request = $Tinkoff->buildQuery('Init', $arrFields);
+        $request = json_decode($request);
 
-            if (isset($request->PaymentURL)) {
-                $button = '<a class="checkout_button" style="display: inline-block" href="'.$request->PaymentURL.'">'.$button_text.'</a>';
-            } else {
-                $button = 'Запрос к сервису ТКС провален';
-            }
+        // Пороверим, не закончился ли поток данных от Тинькофф по причине того, что оплата уже прошла.
+        // Если оплата уже прошла, вернётся код ошибки "8"
+        if (isset($request->Success) && isset($request->ErrorCode) && $request->ErrorCode == 8) {
+            // Отправляем пользователя на страницу заказа
+            $orderUrl = $this->config->root_url . '/order/' . $order->url;
+            $button = '<p>Платёж принят. <a href="' . $orderUrl . '" title="Если не сработало автоматическое перенаправление, то так можно перейти к заказу вручную">Перейти к просмотру заказа</a></p>';
+            $button .= '<script>
+                if (window.location.href != ' . $orderUrl . ') {
+                    window.location.href = "' . $orderUrl . '";
+                }
+                </script>';
+        } else if (isset($request->PaymentURL)) {
+            $button = '<a class="checkout_button btn btn-success btn-lg" href="'.$request->PaymentURL.'">'.$button_text.'</a><br>';
+        } else {
+            $button = 'Запрос к сервису ТКС провален<br>';
         }
 
         return $button;
--- callback.php
+++ callback.php
@@ -1,4 +1,8 @@
 <?php
+chdir ('../../');
+require_once('api/Simpla.php');
+$simpla = new Simpla();
+
 set_error_handler('exceptions_error_handler', E_ALL);
 function exceptions_error_handler($severity) {
     if (error_reporting() == 0) {
@@ -8,11 +12,8 @@ function exceptions_error_handler($severity) {
         die('NOTOK');
     }
 }
-try{
-    chdir ('../../');
-    require_once('api/Simpla.php');
-    $simpla = new Simpla();
 
+try {
     $order = $simpla->orders->get_order(intval($_POST['OrderId']));
     $method = $simpla->payment->get_payment_method(intval($order->payment_method_id));
     $settings = unserialize($method->settings);
@@ -25,17 +26,14 @@ try{
     $values = implode('', array_values($sorted));
     $token = hash('sha256', $values);
 
-//    //log
-//    $log = $_POST;
-//    $log['token'] = $token;
-//    $log['original_token'] = $original_token;
-//    file_put_contents('log.txt', date('Y.m.d H:i:s') . ' ' . json_encode($log)."\r\n",FILE_APPEND);
+    if ($token == $original_token) {
 
-    if($token == $original_token){
-
-        if($_POST['Status'] == 'AUTHORIZED' && $order->status == '2'){
+        if ($_POST['Status'] == 'AUTHORIZED' && $order->paid) {
             die('OK');
         }
+
+        $isNeedChangeOrderStatus = false;
+
         switch ($_POST['Status']) {
             case 'AUTHORIZED': $order_status = '1'; break; /*Деньги на карте захолдированы. Корзина очищается.*/
             case 'CONFIRMED': $order_status = '2'; break; /*Платеж подтвержден.*/
@@ -45,28 +43,30 @@ try{
             case 'REFUNDED': $order_status = '3'; break; /*Произведен возврат денег клиенту*/
         }
 
-        if($_POST['Status'] == 'CONFIRMED'){
-            $update_array = array('paid' => 1, 'status' => $order_status);
-        }
-        else{
-            $update_array = array('status' => $order_status);
+        if ($_POST['Status'] == 'CONFIRMED') {
+            $update_array = array('paid' => 1);
+            $isNeedChangeOrderStatus = true;
         }
 
         // Установим статус оплачен
         $simpla->orders->update_order(intval($order->id), $update_array);
-        // Отправим уведомление на email
-        $simpla->notify->email_order_user(intval($order->id));
-        $simpla->notify->email_order_admin(intval($order->id));
+
+        if ($isNeedChangeOrderStatus) {
+            // Установим статус заказа
+            $simpla->orders->update_order(intval($order->id), ['status' => '1']);
+        }
 
         // Спишем товары
         $simpla->orders->close(intval($order->id));
 
+        // Отправим уведомление на email
+        $simpla->notify->email_order_user(intval($order->id));
+        $simpla->notify->email_order_admin(intval($order->id));
+
         die('OK');
-    }
-    else{
+    } else {
         die('NOTOK');
     }
-}
-catch(Exception $e){
+} catch(Exception $e) {
     die('NOTOK');
 }
